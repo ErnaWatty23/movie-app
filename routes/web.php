d0eef0b8ad8cbdadbd3c90fc19a5d50cf08ff9ac
Erna wati <ernaawatty230506@gmail.com>
2024-05-21 22:35:23 +0700

    upload

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,26 @@
 <?php
 
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/movies', [MovieController::class, 'index']);
+
 
 Route::get('/product', function () {
     return view('product');
 });
 
-Route::get('/movies', function () {
-    return view('movies.index');
-});
-
 Route::get('/create', function () {
     return view('movies.create');
 });
+
+Route::get('/genres', [MovieController::class, 'index']);
 
 Route::get('/genres', function () {
     return view('genres.index');
@@ -26,3 +29,9 @@ Route::get('/genres', function () {
 Route::get('/reviews', function () {
     return view('reviews.index');
 });
+
+
+Route::get('/genres', [GenreController::class, 'index']);
+Route::get('/reviews', [ReviewController::class, 'index']);
+
+
